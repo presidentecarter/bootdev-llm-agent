c64b417c2c8f34f052d57b3d2e844840d80a65a1
Carter Boyle <carter.boyle2@gmail.com>
2026-05-25 07:25:46 -0400

    Hello world for agent

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("no GEMINI_API_KEY found")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.", model='gemini-3.5-flash')
+    print(response.text)
 
 if __name__ == "__main__":
     main()
