3c97281f1eb70a5637eede62a98a607be69f9f69
Carter Boyle <carter.boyle2@gmail.com>
2026-05-26 08:10:56 -0400

    add parser for arguments passing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     load_dotenv()
@@ -10,7 +11,11 @@ def main():
         raise RuntimeError("no GEMINI_API_KEY found")
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.", model='gemini-3.5-flash')
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(contents=args.user_prompt, model='gemini-3.5-flash')
     if not response.usage_metadata:
         raise RuntimeError("No LLM response received")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
