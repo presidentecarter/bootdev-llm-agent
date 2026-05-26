fb80d35da2831128dbf08218fd8754245c5a0863
Carter Boyle <carter.boyle2@gmail.com>
2026-05-26 08:04:16 -0400

    add token counts

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.", model='gemini-3.5-flash')
+    if not response.usage_metadata:
+        raise RuntimeError("No LLM response received")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 if __name__ == "__main__":
