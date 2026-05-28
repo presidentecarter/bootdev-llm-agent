6f8598ea1de5e45864241b35215e526ba538228a
Carter Boyle <carter.boyle2@gmail.com>
2026-05-28 07:05:19 -0400

    Add get files tool and tests

---
+++ functions/get_files_info.py
@@ -0,0 +1,16 @@
+# functions/get_files_info.py
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+    valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+    try:
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        return f'Success: "{directory}" is within the working directory'
+    except Exception as e:
+            return f'Error: {e}'
+
