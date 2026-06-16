bb1c9fa3e5efd21fbff0fdca435bbeadcbbc9ee4
Carter Boyle <carter.boyle2@gmail.com>
2026-06-16 14:41:56 -0400

    Add looping functionality for the agent to perform multiple tool calls and understand context of those tool calls

--- main.py
+++ main.py
@@ -11,6 +11,36 @@ from functions.get_file_content import schema_get_file_content
 from functions.write_file import schema_write_file
 from functions.run_python_file import schema_run_python_file
 
+
+def process_response(response, args):
+    if not response.usage_metadata:
+        raise RuntimeError("No LLM response received")
+    if args.verbose:
+        print(f"User prompt: {response.prompt_feedback}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    function_results = []
+    if response.function_calls:
+        for call in response.function_calls:
+            function_call_result = call_function(call)
+
+            if not function_call_result.parts:
+                raise Exception(f"function call {call.name} returned empty .parts")
+            if not function_call_result.parts[0].function_response:
+                raise Exception(f"function call {call.name} contained response of None")
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception(f"function call {call.name} had no response")
+
+            function_results.append(function_call_result.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+    else:
+        print(response.text)
+
+    return function_results
+
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -36,36 +66,27 @@ def main():
                                        system_instruction=system_prompt,
                                        temperature=0)
 
-    response = client.models.generate_content(
-        contents=messages,
-        model='gemini-3.5-flash',
-        config=config
-    )
+    for _ in range(20):
+        response = client.models.generate_content(
+            contents=messages,
+            model='gemini-2.5-flash',
+            config=config
+        )
 
-    if not response.usage_metadata:
-        raise RuntimeError("No LLM response received")
-    if args.verbose:
-        print(f"User prompt: {response.prompt_feedback}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates:
+            for c in response.candidates:
+                if c.content:
+                    messages.append(c.content)
 
-    function_results = []
-    if response.function_calls:
-        for call in response.function_calls:
-            function_call_result = call_function(call)
+        function_results = process_response(response, args)
 
-            if not function_call_result.parts:
-                raise Exception(f"function call {call.name} returned empty .parts")
-            if not function_call_result.parts[0].function_response:
-                raise Exception(f"function call {call.name} contained response of None")
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception(f"function call {call.name} had no response")
-
-            function_results.append(function_call_result.parts[0])
-
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    print(response.text)
+        if function_results:
+            messages.append(types.Content(role="user", parts=function_results))
+        else: 
+            exit(0)
+    
+    print("maximum number of iterations reached")
+    exit(1)
 
 if __name__ == "__main__":
     main()
